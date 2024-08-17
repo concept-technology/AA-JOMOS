d516705bbe38403abac5c422b5245230523c3249
andrew samuel <samuel.andrew5804@gmail.com>
2024-08-17 09:59:46 +0100

    changed database

--- aa_jomos/settings.py
+++ aa_jomos/settings.py
@@ -113,19 +113,19 @@ if RENDER_EXTERNAL_HOSTNAME:
 else:        
     # Database documentation https://docs.djangoproject.com/en/5.0/ref/settings/#databases
     None
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default=DATABASE_URL,
-#         conn_max_age=600
-#     )
-# }
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=DATABASE_URL,
+        conn_max_age=600
+    )
+}
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
