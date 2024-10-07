1cf6c7b377376e2c64e0541c50756e110a4ab5f4
concept-technology <andrewsamuelcloud@gmail.com>
2024-10-07 05:30:49 +0100

    fixed version bjjfdfddd

--- aa_jomos/settings.py
+++ aa_jomos/settings.py
@@ -2,8 +2,6 @@
 import os
 from pathlib import Path
 import environ
-import dj_database_url
-from twilio.rest import Client
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
