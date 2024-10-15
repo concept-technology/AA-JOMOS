3792e14be1bfb493fa176b7ad5547d5e4e65ef24
concept-technology <162841525+concept-technology@users.noreply.github.com>
2024-10-15 16:16:57 +0100

    Update wsgi.py

--- aa_jomos/wsgi.py
+++ aa_jomos/wsgi.py
@@ -3,7 +3,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-setting_module = 'aa_jomos.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'aa_jomos.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aa_jomos.settings')
 
