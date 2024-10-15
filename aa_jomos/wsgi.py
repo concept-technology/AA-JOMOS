8f1ddcacdd5fc95d91da264c139c898d7e331230
concept-technology <andrewsamuelcloud@gmail.com>
2024-10-15 23:30:49 +0100

    added server setting

--- aa_jomos/wsgi.py
+++ aa_jomos/wsgi.py
@@ -2,9 +2,9 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+production = False
+setting_module = 'aa_jomos.settings' if production else 'aa_jomos.deployment'
 
-setting_module = 'aa_jomos.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'aa_jomos.settings'
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aa_jomos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', setting_module)
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -2,11 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from aa_jomos.wsgi import setting_module
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aa_jomos.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', setting_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
